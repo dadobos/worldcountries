e13571fe702803f152a5fc3237044b0e10b48222
Dan Dobos <dan@prowarehouse.nl>
2025-10-17 11:26:13 +0200

    chore: clean up deploy script by removing commented sections and improving clarity

--- deploy.sh
+++ deploy.sh
@@ -9,16 +9,12 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
+# create a fresh repo in dist and push to gh-pages
+git init
+git checkout -b gh-pages
 git add -A
-git commit -m 'deploy'
+git commit -m 'deploy' || true
+git remote add origin git@github.com:dadobos/worldcountries.git
+git push -f origin gh-pages
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:dadobos/worldcountries.git main:gh-pages
-# git push -f git@github.com:<USERNAME>/world.git main:gh-pages
 cd -
